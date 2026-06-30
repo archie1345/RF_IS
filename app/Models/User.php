47c7ee3f005a0d1a949b905ae2d734f84c36228c
archie1345 <158141499+archie1345@users.noreply.github.com>
2026-06-30 11:57:17 +0700

    Support legacy parent child links (#10)

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -103,16 +102,20 @@ class User extends Authenticatable
         return $this->hasOne(Parents::class, 'id', 'id');
     }
 
-    public function children(): HasManyThrough
+    public function children(): HasMany
     {
-        return $this->hasManyThrough(
-            Athlete::class,
-            Parents::class,
-            'id',
-            'parent_id',
-            'id',
-            'parent_id'
-        );
+        $parentIds = collect([
+            $this->getKey(),
+            $this->parentProfile?->parent_id,
+        ])
+            ->filter(fn ($id) => filled($id))
+            ->map(fn ($id) => (string) $id)
+            ->unique()
+            ->values()
+            ->all();
+
+        return $this->hasMany(Athlete::class, 'parent_id', 'id')
+            ->when(count($parentIds) > 0, fn ($query) => $query->orWhereIn('athletes.parent_id', $parentIds));
     }
 
     public function athleteProfile(): HasOne
