a305e15f1cec11b36a719af2a398961483859f48
archie1345 <158141499+archie1345@users.noreply.github.com>
2026-07-21 15:05:11 +0700

    Fix auto refresh Inertia reload options

--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -35,7 +35,6 @@ function refreshPageData(): void {
 
     try {
         router.reload({
-            preserveScroll: true,
             preserveState: true,
             onFinish: () => {
                 refreshInFlight = false;
