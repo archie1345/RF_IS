82c306d519259d8560b13b6fc78ac240bfe1e9b0
archie1345 <158141499+archie1345@users.noreply.github.com>
2026-07-23 21:19:06 +0700

    feat: include member numbers in fresh seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,13 +6,11 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         $this->call([
             ApplicationDataSeeder::class,
+            MemberNumberSeeder::class,
         ]);
     }
 }
