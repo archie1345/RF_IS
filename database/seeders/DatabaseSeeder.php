ca69bc41c86973278fdd5f5e03ab2fb278c2ba6f
archie1345 <158141499+archie1345@users.noreply.github.com>
2026-07-24 00:26:01 +0700

    refactor: centralize application seeder ordering

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,9 +8,6 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        $this->call([
-            ApplicationDataSeeder::class,
-            MemberNumberSeeder::class,
-        ]);
+        $this->call(ApplicationDataSeeder::class);
     }
 }
