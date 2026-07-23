20b0e323c35ab29590778229571d4b48bd80b6c2
archie1345 <158141499+archie1345@users.noreply.github.com>
2026-07-24 00:27:10 +0700

    style: format database audit command

--- routes/console.php
+++ routes/console.php
@@ -65,13 +65,14 @@ Artisan::command('app:database-audit', function () {
     $missingColumns = [];
 
     foreach ($requirements as $table => $columns) {
-        if (! Schema::hasTable($table)) {
+        if (!Schema::hasTable($table)) {
             $missingTables[] = $table;
+
             continue;
         }
 
         foreach ($columns as $column) {
-            if (! Schema::hasColumn($table, $column)) {
+            if (!Schema::hasColumn($table, $column)) {
                 $missingColumns[] = "{$table}.{$column}";
             }
         }
