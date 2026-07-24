4761a98e3677ac5af97db0feabbb065f8db770c0
archie1345 <158141499+archie1345@users.noreply.github.com>
2026-07-24 17:37:09 +0700

    seed: include default payment and QRIS configuration

--- database/seeders/ApplicationDataSeeder.php
+++ database/seeders/ApplicationDataSeeder.php
@@ -11,6 +11,7 @@ class ApplicationDataSeeder extends Seeder
         $this->call([
             FreshApplicationSeeder::class,
             AllRoleDemoSeeder::class,
+            PaymentConfigurationSeeder::class,
             FinanceLedgerDemoSeeder::class,
             MemberNumberSeeder::class,
         ]);
