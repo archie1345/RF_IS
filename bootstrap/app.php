4bcfa89775e117c35971d1f229355e75db502f33
archie1345 <158141499+archie1345@users.noreply.github.com>
2026-07-24 16:57:35 +0700

    security: register role authorization middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\EnsureAccountIsActive;
+use App\Http\Middleware\EnsureUserHasRole;
 use App\Http\Middleware\HandleAppearance;
 use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Application;
@@ -23,6 +24,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->alias([
             'account.active' => EnsureAccountIsActive::class,
+            'role' => EnsureUserHasRole::class,
         ]);
 
         $middleware->web(append: [
