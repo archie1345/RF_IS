2370c9b664b7f2182138991b5dc9b3dd9e0e130a
archie1345 <158141499+archie1345@users.noreply.github.com>
2026-07-25 14:31:16 +0700

    fix: constrain sidebar logo within its container

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -3,5 +3,11 @@ import AppLogoIcon from '@/components/AppLogoIcon.vue';
 </script>
 
 <template>
-        <AppLogoIcon/>
+    <span
+        class="flex h-10 min-w-0 w-full max-w-full items-center justify-center overflow-hidden group-data-[collapsible=icon]:h-8 group-data-[collapsible=icon]:w-8"
+    >
+        <AppLogoIcon
+            class-name="block max-h-full max-w-full shrink object-contain object-center"
+        />
+    </span>
 </template>
